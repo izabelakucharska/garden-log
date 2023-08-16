a5e59e3181dffae990b8e420a1e84d8b9a3d76d4
izabelakucharska <karwowskaizabela0@gmail.com>
2023-08-16 21:31:28 +0900

    add Home component

---
+++ frontend/src/home/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.css';
+export default function Home() {
+  return (
+    <div  className="home-page">
+      <h1>Welcome to Garden Log!</h1>
+      <p>You can create your own garden here</p>
+      <p>You can track the condition of your plants and watch how others manage their garden</p>
+    </div>
+  )
+}
