5e6f5b9400b954e1f256dcc2c98ec173dd2996bf
izabelakucharska <karwowskaizabela0@gmail.com>
2023-08-16 21:31:55 +0900

    update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
+// import {Cloudinary} from "@cloudinary/url-gen";
+// import {AdvancedImage} from '@cloudinary/react';
+// import {fill} from "@cloudinary/url-gen/actions/resize";
 import './App.css';
 import Signup from './signup/Signup';
 import Login from './login/Login';
 import Nav from './navigation/Nav';
 import MyGarden from './mygarden/MyGarden';
+import Home from './home/Home'
 import { logout, loginStatus } from './util/fetchRequests';
 
 
@@ -11,6 +15,12 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(true)
   const [user, setUser] = useState({ name: 'None', email: 'none@null.com', id: 0 })
   const [page, setPage] = useState("home")
+
+  // const cld = new Cloudinary({
+  //   cloud: {
+  //     cloudName: 'demo'
+  //   }
+  // })
 
   async function exit() {
     const logoutStatus = await logout()
@@ -36,13 +46,13 @@ function App() {
   function renderPage() {
     switch(page) {
       case 'home':
-        return <div></div>
+        return <Home />
       case 'signup':
         return <Signup setPage={setPage} />
       case 'login' :
         return <Login setPage={setPage} fetchLogin={fetchLogin} />
       case 'mygarden':
-        return <MyGarden/>
+        return <MyGarden />
     }
   }
 
